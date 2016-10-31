5a40f406a6b1bdf48538f79fd4a57643eb5083bd
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-31 16:31:07 -0400

    I have the started playing with the previous button click

--- js/cat.js
+++ js/cat.js
@@ -7,9 +7,12 @@
         $(this).toggleClass('zoomed');
         $('main').toggleClass('zooming');
         $(this).append($ ('nav') );
-
-
-
 })
+
+  $('nav')
+    .find('button')
+      .on('click', function previousPic(event){
+      console.log('hello!');
+      });
 
 })();
