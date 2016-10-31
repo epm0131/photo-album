0d187c9727d0e4bf33851226099e229e46ab8f82
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-31 15:11:50 -0400

    I added the correct toggle for zoom and zooming

--- js/cat.js
+++ js/cat.js
@@ -1,45 +1,14 @@
-(function() {
+(function () {
   'use strict';
 
   $('.photos')
     .find('li')
-    .on('click', function zoomIn(event) {
-      $(this).toggleClass('zoomed');
-
-
-    });
-
+      .on('click', function zoomIn(event) {
+        $(this).toggleClass('zoomed');
+        $('main').toggleClass('zooming');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+})
 
 })();
