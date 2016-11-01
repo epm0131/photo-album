357bdf2162da15f07aab614aea408f02c3cd7f18
Ettore Merendino <merendino.ettore@gmail.com>
2016-11-01 08:50:31 -0400

    I was attempting to have the zoom out happen when you arrived to the last or first picture

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# photo-album
+The Iron Yard
+
+Photo Album Project
+
+The purpose of this assignment is to be able to zoom in images and be able to scroll through them in the zoomed state.  
+Modifying the toggle was the method used to help zoom in and out of the images.
--- js/cat.js
+++ js/cat.js
@@ -17,25 +17,19 @@
         var currentImage = $(this).closest('li')
         var previousImage = $(this).closest('li').prev();
         var nextImage = $(this).closest('li').next();
+        var lastImage = $(this).last();
 
         if ($(this).is('.previous')) {
           previousImage.append($ ('nav') );
           previousImage.toggleClass('zoomed');
           currentImage.removeClass('zoomed');
         }
+
         else {
           nextImage.append($ ('nav') );
           nextImage.toggleClass('zoomed');
           currentImage.removeClass('zoomed');
         }
-
-
-
-        
-
-
-
-
-      });
+});
 
 })();
